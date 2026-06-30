4a7d2f73e5acee68edc24c94cc3d2ac6fd12452f
Anuraj Mane <anurajmane04@gmail.com>
2026-06-30 13:51:32 +0530

    feat: implement JWT authentication and authorization with login endpoint, security filters, and user details service

--- kanban-backend/kanban-backend/src/main/java/com/anuraj/kanban/auth/controller/AuthController.java
+++ kanban-backend/kanban-backend/src/main/java/com/anuraj/kanban/auth/controller/AuthController.java
@@ -1,13 +1,14 @@
 package com.anuraj.kanban.auth.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anuraj.kanban.auth.dto.AuthResponse;
+import com.anuraj.kanban.auth.dto.LoginRequest;
 import com.anuraj.kanban.auth.dto.RegisterRequest;
 import com.anuraj.kanban.auth.service.AuthService;
 
@@ -27,5 +28,11 @@ public class AuthController {
 		return ResponseEntity
 				.status(HttpStatus.CREATED)
 				.body("User registered successfully..!");
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
+		AuthResponse response = authService.login(request);
+		return ResponseEntity.ok(response);
 	}
 }
--- kanban-backend/kanban-backend/src/main/java/com/anuraj/kanban/config/SecurityConfig.java
+++ kanban-backend/kanban-backend/src/main/java/com/anuraj/kanban/config/SecurityConfig.java
@@ -2,18 +2,34 @@ package com.anuraj.kanban.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.anuraj.kanban.security.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(
+			AuthenticationConfiguration config) throws Exception{
+		return config.getAuthenticationManager();
 	}
 	
 	@Bean
@@ -27,7 +43,7 @@ public class SecurityConfig {
 				.anyRequest()
 				.authenticated()
 		)
-		.httpBasic(Customizer.withDefaults());
+		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
 	}
